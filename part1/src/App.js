ca4d71f635af9b678726843356f55ab0f794979f
Wdson Danrlei <44691537+wdson91@users.noreply.github.com>
2026-06-18 16:20:23 +0100

    1.6 - 1.14

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,35 +1,68 @@
+import { useState } from 'react'
 
-import Header from "./Header"
-import Content from "./Content"
-import Total from "./Total"
+const Button = (props) => {
 
-const App = () => {
-  const course = {
-    name: 'Desenvolvimento de aplicação Half Stack',
-    parts: [
-      {
-        name: 'Fundamentos da biblioteca React',
-        exercises: 10
-      },
-      {
-        name: 'Usando props para passar dados',
-        exercises: 7
-      },
-      {
-        name: 'Estado de um componente',
-        exercises: 14
-      }
-    ]
-  }
 
   return (
+    <button onClick={props.value}>
+      {props.text}
+    </button>
+  )
+
+}
+
+
+
+const StatisticLine = ({ text, value }) => {
+
+
+
+
+  return (
+    <tr>
+      <td>{text} : {value}</td>
+    </tr>
+  )
+
+}
+
+
+
+
+const App = () => {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  let total = good + bad + neutral
+
+  const average = total > 0 ? (good - bad) / total : 0
+
+
+  const positive = total > 0 ? (good / total) * 100 : 0
+
+
+  return (
+
     <div>
+      <h1>Give FeedBack</h1>
+      <Button value={() => setGood(good + 1)} text="Good" />
+      <Button value={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button value={() => setBad(bad + 1)} text="Bad" />
 
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <div><h1>Statistics</h1></div>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={` ${positive} %`} />
+        </tbody>
+      </table>
 
-    </div>
+    </div >
   )
 }
 
